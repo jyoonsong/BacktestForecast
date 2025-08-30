b135dbcc837a810fafcd80abe68d64e510295e58
jyoonsong <jaeyoons@mit.edu>
2025-08-30 00:35:34 -0400

    run ddgs report generation only if it is not already there

--- scrape-kalshi.py
+++ scrape-kalshi.py
@@ -213,7 +213,8 @@ def scrape_kalshi_events():
             # TODO: add a timestamped research report for this event
             found_event = next((e for e in current_events if e['event_ticker'] == event['event_ticker']), None)
             event['bing_reports'] = ""
-            event['ddgs_reports'][timestamp] = get_ddgs_report(found_event)
+            if timestamp not in event.get('ddgs_reports', {}):
+                event['ddgs_reports'][timestamp] = get_ddgs_report(found_event)
             # save the event
             final_events.append(event)
             
@@ -229,7 +230,8 @@ def scrape_kalshi_events():
             # TODO: add a timestamped research report for this event
             event_obj = {}
             event_obj['bing_reports'] = ""
-            event_obj['ddgs_reports'] = {timestamp: get_ddgs_report(event)}
+            if timestamp not in event.get('ddgs_reports', {}):
+                event_obj['ddgs_reports'] = {timestamp: get_ddgs_report(event)}
             # save the event
             event_obj['event_ticker'] = event['event_ticker']
             event_obj['series_ticker'] = event['series_ticker']
