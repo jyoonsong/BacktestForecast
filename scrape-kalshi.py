274b6c7d940f73922fc7dde9d15d3003c996c250
jyoonsong <jaeyoons@mit.edu>
2025-08-31 17:56:45 -0400

    temporary script for resolved events

--- scrape-kalshi.py
+++ scrape-kalshi.py
@@ -185,26 +185,6 @@ def scrape_kalshi_events():
                         # save hash id in events.json
                         event['ddgs_reports'][timestamp] = hash_id
 
-                # TEMPORARY: already present 
-                else:
-                    # generate unique hash id
-                    hash_id = f"ddgs_{event['event_ticker'].lower()}_{timestamp}"
-
-                    # if hash id is not in ddgs_reports, fetch the report from DB
-                    if not any(r['hash_id'] == hash_id for r in ddgs_reports):
-                        report = read_from_db(timestamp, event["event_ticker"])
-
-                        # move it to reports.json
-                        ddgs_reports.append({
-                            "hash_id": hash_id,
-                            "timestamp": timestamp,
-                            "event_ticker": event["event_ticker"],
-                            "report": report,
-                        })
-
-                        # save hash id instead
-                        event['ddgs_reports'][timestamp] = hash_id
-
             # Keep the event active.
             final_events.append(event)
             
@@ -308,6 +288,29 @@ def scrape_kalshi_events():
             logger.info(f"Market {market['ticker']} is no longer active.")
             market['resolution_date'] = timestamp_now
             resolved_markets.append(market)
+
+    # TEMPORARY script for moving reports from events to reports.json
+    for event in resolved_events:
+        for timestamp in timestamps:
+            if timestamp in event["ddgs_reports"]:
+                # generate unique hash id
+                hash_id = f"ddgs_{event['event_ticker'].lower()}_{timestamp}"
+
+                # if hash id is not in ddgs_reports, fetch the report from DB
+                if not any(r['hash_id'] == hash_id for r in ddgs_reports):
+                    # report = read_from_db(timestamp, event["event_ticker"])
+
+                    # move it to reports.json
+                    ddgs_reports.append({
+                        "hash_id": hash_id,
+                        "timestamp": timestamp,
+                        "event_ticker": event["event_ticker"],
+                        "report": event['ddgs_reports'][timestamp],
+                    })
+
+                    # replace report with hash id instead
+                    event['ddgs_reports'][timestamp] = hash_id
+
 
     # Persist updated snapshots to disk.
     with open(files[0], "w") as f:
