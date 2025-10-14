d724b63eb45998e91d16891808cb06dc7be66244
jyoonsong <jaeyoons@mit.edu>
2025-10-14 14:02:46 -0400

    more logging and retry logic in main.py

--- kalshi_ddgs_rag/main.py
+++ kalshi_ddgs_rag/main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import time
 from .utils import log, utc_stamp
 from .db import read_from_db, write_to_db
 from .events import fetch_sampled_events
@@ -20,19 +21,27 @@ def main():
             log(f"Already exists: {ticker}, skipping.")
             continue
 
-        try:
-            resp = requests.get(
-                f"https://api.elections.kalshi.com/trade-api/v2/events/{ticker}?with_nested_markets=true",
-                timeout=15,
-            )
-            event = resp.json().get("event", {})
-            if not event.get("markets"):
-                continue
-            report, contents = get_ddgs_report(event)
-            write_to_db(report, contents, timestamp, ticker)
-        except Exception as err:
-            log(f"Failed processing {ticker}: {err}")
-
+        event = None
+        trials = 0
+        while event == None and trials < 5:
+            try:
+                resp = requests.get(
+                    f"https://api.elections.kalshi.com/trade-api/v2/events/{ticker}?with_nested_markets=true",
+                    timeout=15,
+                )
+                event = resp.json().get("event", {})
+                if not event.get("markets"):
+                    log(f"No markets for {ticker}, skipping.")
+                    event = None
+                    continue
+            except Exception as err:
+                log(f"Error fetching event {ticker}: {err}")
+                trials += 1
+                time.sleep(3)
+        
+        report, contents = get_ddgs_report(event)
+        write_to_db(report, contents, timestamp, ticker)
+        
     log("Report generation completed.")
 
 if __name__ == "__main__":
