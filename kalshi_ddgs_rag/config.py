36a4869e94bf6a231b4a65ea8de303708fe1f164
jyoonsong <jaeyoons@mit.edu>
2025-10-14 20:32:06 -0400

    remove unused config variable

--- kalshi_ddgs_rag/config.py
+++ kalshi_ddgs_rag/config.py
@@ -7,7 +7,6 @@ from pymongo import MongoClient
 # -----------------------------------------------------------------------------
 
 MODEL_NAME = "gpt-4o-mini-2024-07-18"
-TARGET_EVENTS = 200
 NUM_QUERIES = 6
 NUM_URLS = 5
 MAX_QUERY_WORDS = 7
